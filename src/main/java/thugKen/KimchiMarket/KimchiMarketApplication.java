779db9fd3a196eb12ca38122573144816874f5b8
kenryu <jryu1521@gmail.com>
2024-03-11 16:10:48 +0900

    init

---
+++ src/main/java/thugKen/KimchiMarket/KimchiMarketApplication.java
@@ -0,0 +1,13 @@
+package thugKen.KimchiMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimchiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimchiMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thugKen/KimchiMarket/KimchiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package thugKen.KimchiMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimchiMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
